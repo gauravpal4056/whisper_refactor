1da4bf2acd8697b7018d405d8c1bffbcfbdc16b6
Gaurav Pal <gauravpal95106@gmail.com>
2023-07-16 09:19:04 +0530

    login refractor

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -5,16 +5,17 @@ const Login = () => {
         <>
             <Box className="h-screen w-screen flex md:flex-row flex-col gap-3 bg-[#f0f1f5] p-0 ">
                 <Box>
-                    <Box className="flex items-center gap-3">
-                        <div className=" h-14 w-14 bg-[url('/logo.png')] bg-cover bg-center " />
-                        <h1 className="text-black text-xl uppercase font-extrabold">Whispers</h1>
+                    <Box className="flex items-center gap-1 m-3">
+                        <div className=" h-12 w-12 bg-[url('/logo.png')] bg-cover bg-center " />
+                        <h1 className="text-[#3e3e3e] text-lg uppercase font-bold">Whispers</h1>
                     </Box>
                     <Container maxWidth="sm" className="py-10 md:mt-20 md:w-[760px] flex flex-col items-center justify-center text-black">
-                        <Paper elevation={3} className="p-5 md:p-10 flex flex-col gap-1" variant="outlined">
-                            <h1 className="text-6xl text-[#e80384] my-4">Get Started </h1>
-                            <Box><h1 className="text-3xl w-full font-extrabold/v">Welcome Back!</h1></Box>
+                        <Paper elevation={2} className="p-5 md:p-10 flex flex-col gap-1" >
+                            <h1 className="text-6xl text-[#e80384] my-5 font-extrabold">Get Started </h1>
+                            <Box><h1 className="text-3xl w-full font-bold my-2 text-center">Welcome Back!</h1></Box>
                             <h1 className="text-lg text-gray-500">Login or Signup with Google </h1>
-                            <button className="text-white my-1 bg-[#e80384] flex items-center "><img className="w-5 h-5" src={src} /><h1 className="text-center w-full text-xl">Login/signup</h1></button>
+                            
+                            <button className="text-white my-1 bg-[#e80384] flex items-center "><img className="w-5 h-5" src={src} /><h1 className="text-center w-full text-xl">Login or Signup</h1></button>
                         </Paper>
                     </Container>
                 </Box>
